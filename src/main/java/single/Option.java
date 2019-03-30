0ac204a7845e9ce2e01b1c86b340c265f11a7f48
Ricfilipe <ricardo.l.filipe@tecnico.ulisboa.pt>
2019-03-30 19:44:09 +0000

    #3 close Comando a funcionar
    Signed-off-by: Ricfilipe <ricardo.l.filipe@tecnico.ulisboa.pt>

--- src/main/java/single/Option.java
+++ src/main/java/single/Option.java
@@ -10,45 +10,46 @@ public class Option implements NamedClasse {
     public boolean isObservation;
     public int value;
     public String name;
-    public Option( String name){
-        this.name =name;
+
+    public Option(String name) {
+        this.name = name;
 
     }
 
-    public boolean isFinal(){
+    public boolean isFinal() {
         return options.isEmpty();
     }
 
-    public String toString(){
-        String buffer= name +"=(";
-        if(isObservation){
-            buffer=buffer+Observed;
-        }else{
-            buffer=buffer+belief+"%";
+    public String toString() {
+        String buffer = name + "=(";
+        if (isObservation) {
+            buffer = buffer + Observed;
+        } else {
+            buffer = buffer + belief + "%";
         }
-        if(!isFinal()){
-            buffer=buffer + ",[";
-            for(int i = 0; i< options.size(); i++){
-                if(i!=0){
-                    buffer= buffer +",";
+        if (!isFinal()) {
+            buffer = buffer + ",[";
+            for (int i = 0; i < options.size(); i++) {
+                if (i != 0) {
+                    buffer = buffer + ",";
                 }
-               buffer=buffer + options.get(i).toString();
+                buffer = buffer + options.get(i).toString();
 
             }
-            buffer=buffer+"]";
-        }else{
-            buffer=buffer+","+value;
+            buffer = buffer + "]";
+        } else {
+            buffer = buffer + "," + value;
         }
-        return buffer+")";
+        return buffer + ")";
     }
 
     public double getTotalObersavation() throws Exception {
-        double total=0;
-        for(Option op : options){
-            if(!op.isObservation){
+        double total = 0;
+        for (Option op : options) {
+            if (!op.isObservation) {
                 throw new Exception();
             }
-            total+= op.Observed;
+            total += op.Observed;
         }
         return total;
     }
